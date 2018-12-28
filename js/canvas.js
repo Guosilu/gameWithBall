18446547ff2022a19a435b211f79309f5d473acc
0d4_100003690587_13156007572 <0d4_100003690587_13156007572@git.cloud.tencent.com>
2018-12-28 16:14:22 +0800

    项目开始

---
+++ js/canvas.js
@@ -0,0 +1,40 @@
+
+(function () {
+
+
+
+    var ctx = document.getElementById('canvas');
+    var canvas = ctx.getContext('2d');
+
+    // requestAnimationFrame(run(0));
+    //
+    //
+    // function run(a) {
+    //     a++
+    //     let image = createImg('image/bullet.png');
+    //     console.log(image);
+    //     image.onload = function () {
+    //         canvas.drawImage(image,0,0,image.width,image.height,a,a,image.width,image.height);
+    //     }
+    //     if(a>window.innerWidth){
+    //         a=0;
+    //     }
+    //
+    //     requestAnimationFrame(run(a));
+    // }
+
+    function createImg(src){
+        var img = new Image();
+        img.src=src;
+        return img;
+    }
+
+
+
+
+
+
+
+})()
+
+
